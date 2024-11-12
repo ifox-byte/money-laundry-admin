12cefc0904f03bd8887563eb7e0ed18f535b23ce
ifox-byte <j4130331@gmail.com>
2024-11-12 17:52:51 +0700

    finish phase 6 : code standardization

--- pages/templates/index.tsx
+++ pages/templates/index.tsx
@@ -2,3 +2,7 @@ export {default as Login} from "@/pages/templates/login"
 export {default as Home} from "@/pages/templates/home"
 export {default as Sidebar} from "@/pages/templates/sidebar"
 export {default as Table} from "@/pages/templates/table"
+
+const Templates = () => null
+
+export default Templates
